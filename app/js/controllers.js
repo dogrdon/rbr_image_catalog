da32d178597260bfe47cdc96c169295cbae46945
droquo <drew.s.gordon@gmail.com>
2014-09-02 18:02:09 -0400

    do files w/o ng-flow

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,5 +7,12 @@ angular.module('myApp.controllers', [])
 
   }])
   .controller('imageUploadCtrl', ['$scope', function($scope) {
+    this.imageSubmit = function(image){
 
+        //add image upload logic for submit, still needs 'backend', this obviously doesn't submit diddly
+        alert(image + ': thanks for adding.')
+        
+      };
   }]);
+
+ 
