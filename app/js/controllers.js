e89dd9bd8010fcb2bb16c2beeb6f91fa04d27442
droquo <drew.s.gordon@gmail.com>
2014-09-02 18:11:55 -0400

    tidy up form, add semantics and styling

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,7 +7,8 @@ angular.module('myApp.controllers', [])
 
   }])
   .controller('imageUploadCtrl', ['$scope', function($scope) {
-    this.imageSubmit = function(image){
+    
+    $scope.imageSubmit = function(image){
 
         //add image upload logic for submit, still needs 'backend', this obviously doesn't submit diddly
         alert(image + ': thanks for adding.')
