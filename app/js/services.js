2e57fa876883fbea5a3873df888ee7cd7b39b7f9
droquo <drew.s.gordon@gmail.com>
2014-09-09 17:42:07 -0400

    add service for dummy data GET using ngResource

--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,16 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+//angular.module('myApp.services', []).
+//  value('version', '0.1');
+
+
+var rbrimageServices = angular.module('rbrimageServices', ['ngResource']);
+
+rbrimageServices.factory('Image', ['$resource',
+	function($resource){
+		return $resource('metadata/dummy.json', {}, {
+			query: {method: 'GET', params:{}, isArray:true}
+		});
+
+	}]);
