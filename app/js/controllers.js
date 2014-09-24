88021df1699f15397acd63350820555b995abdbb
droquo <drew.s.gordon@gmail.com>
2014-09-23 23:39:19 -0400

    crude details controller

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,13 +9,28 @@ angular.module('myApp.controllers', [])
       });
   }])
 
-  .controller('imageUploadCtrl', ['$scope', function($scope) {
+  .controller('imageUploadCtrl', ['$scope', 
+  	function($scope) {
     
-    $scope.imageSubmit = function(image){
+    	$scope.imageSubmit = function(image){
 
-        //add image upload logic for submit, still needs 'backend', this obviously doesn't submit diddly
-        alert(image + ': thanks for adding.')
+        	//add image upload logic for submit, still needs 'backend', this obviously doesn't submit diddly
+        	alert(image + ': thanks for adding.')
         
-      };
+      	};
+  }])
+  
+  .controller('imageDetailCtrl', ['$scope', '$http', '$routeParams', 
+  	function($scope, $http, $routeParams){
+		$http.get('metadata/dummy.json').success (function(data){
+			$scope.imageVar = data;
+			$scope.thisImage = $routeParams.imageId;
+			
+			console.log('hi i am ' + $scope.imageVar);
+			console.log('and my name is ' + $scope.thisImage); //item._id
+			
+		});
+		
+  
   }]);
 
